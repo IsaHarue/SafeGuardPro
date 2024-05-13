a304efbdf5709cd221b9c8b91926453c05864df7
Isabelle Harue Tsutsumi <isabelle.tsutsumi@portalsesisp.org.br>
2024-05-13 16:59:23 -0300

    adapter

---
+++ app/src/main/java/com/hpe/safeguardpro/service/model/Pessoa.kt
@@ -0,0 +1,5 @@
+package com.hpe.safeguardpro.service.model
+
+class Pessoa {
+
+}
---
+++ app/src/main/java/com/hpe/safeguardpro/view/adapter/ItemEpiAdapter.kt
@@ -0,0 +1,48 @@
+package com.hpe.safeguardpro.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hpe.safeguardpro.databinding.FragmentListItemEpiBinding
+import com.hpe.safeguardpro.databinding.FragmentListItemPessoaBinding
+import com.hpe.safeguardpro.service.model.Epi
+import com.hpe.safeguardpro.service.model.Pessoa
+
+class ItemEpiAdapter(Epis: List<Epi>?, private val clickListListener: (Epi) -> Unit) :
+    RecyclerView.Adapter<ItemEpiAdapter.EpiViewHolder>() {
+
+    private var itemEpiList: List<Epi> = arrayListOf()
+
+    class EpiViewHolder(private val binding: FragmentListItemEpiBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        //carrega as informacoes da pessoa na lista
+        fun bind(epi: Epi, clickListListener: (Epi) -> Unit) {
+            //onde meche
+
+            //configura o click de algum item da lista
+            binding.root.setOnClickListener{
+                clickListListener(epi)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EpiViewHolder {
+        val listItemEpiBinding =
+            FragmentListItemEpiBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EpiViewHolder(listItemEpiBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return itemEpiList.count()
+    }
+
+    override fun onBindViewHolder(holder: EpiViewHolder, position: Int) {
+        holder.bind(itemEpiList[position], clickListListener)
+    }
+
+    //carrega a lista de pessoa para serem exibidas
+    fun updateEpis(list: List<Epi>) {
+        itemEpiList = list
+        notifyDataSetChanged()
+    }
+}
