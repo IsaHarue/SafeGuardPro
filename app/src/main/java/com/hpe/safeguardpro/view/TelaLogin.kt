508e9fc3e8fc3f8b497013077c555a11da2301ab
Isabelle Harue Tsutsumi <isabelle.tsutsumi@portalsesisp.org.br>
2024-05-13 08:16:02 -0300

    navegacao

--- app/src/main/java/com/hpe/safeguardpro/view/TelaLogin.kt
+++ app/src/main/java/com/hpe/safeguardpro/view/TelaLogin.kt
@@ -5,56 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.hpe.safeguardpro.R
+import com.hpe.safeguardpro.databinding.FragmentTelaLoginBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TelaLogin.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TelaLogin : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentTelaLoginBinding? = null
+    private val binding: FragmentTelaLoginBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tela_login, container, false)
+    ): View {
+        _binding = FragmentTelaLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TelaLogin.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TelaLogin().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogar.setOnClickListener {
+            findNavController().navigate(R.id.telaInicialFuncionario)
+        }
+
+        binding.tvcriarconta.setOnClickListener {
+            findNavController().navigate(R.id.telaCadastroFuncionario)
+        }
+
     }
 }
